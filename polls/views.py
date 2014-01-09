fbbe3963797dc11e5305f3e5a6366becae63b683
Thomas Dartsch <thomas.dartsch@gmx.de>
2014-01-09 14:08:38 +0100

    Result view implemented.

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from polls.models import Poll, Choice
 
 def index(request):
@@ -13,7 +13,8 @@ def detail(request, poll_id):
     return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll': poll})
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
@@ -28,5 +29,3 @@ def vote(request, poll_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
-
-    return HttpResponse("You're voting on poll %s." % poll_id)
