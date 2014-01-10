600e97b495762b58b1c75743d37790b86052a412
Thomas Dartsch <thomas.dartsch@gmx.de>
2014-01-10 15:20:50 +0100

    Tests for the IndexView.

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
+from django.utils import timezone
 from django.views import generic
 
 from polls.models import Poll, Choice
@@ -10,7 +11,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_poll_list'
 
     def get_queryset(self):
-        return Poll.objects.order_by('-pub_date')[:5]
+        return Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Poll
